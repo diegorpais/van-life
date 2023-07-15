524207502d64231de4a8c2bbc437ef52d673e6f8
Diego Pais <drborges.pais@gmail.com>
2023-07-15 14:02:24 -0300

    (us04): creating mock API and listing the vans

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainLayout from './components/MainLayout/MainLayout';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
+import Vans from './pages/Vans/Vans';
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
           <Route path='/' element={<MainLayout />}>
             <Route index element={<Home />} />
             <Route path='about' element={<About />} />
+            <Route path='vans' element={<Vans />} />
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/pages/Vans/Vans.tsx
@@ -0,0 +1,121 @@
+import { useEffect, useState } from 'react';
+
+import './style.css';
+import { api } from '../../services/api';
+
+interface Van {
+  id: string;
+  name: string;
+  price: number
+  description: string;
+  imageUrl: string;
+  type: string;
+}
+
+export default function Vans() {
+  const filtersType = [
+    'simple',
+    'luxury',
+    'rugged'
+  ];
+
+  const [vans, setVans] = useState<Array<Van>>([]);
+  const [filters, setFilters] = useState<Array<string>>([]);
+
+  useEffect(() => {
+    api.get('vans')
+      .then(res => setVans(res.data))
+  }, []);
+
+  useEffect(() => {
+    filterListOfVans();
+  }, [filters]);
+
+  function filterIsActive(type: string) {
+    const filterIsSelected = filters.filter(filter => filter === type);
+
+    if (filterIsSelected && filterIsSelected.length > 0) {
+      return true;
+    }
+
+    return false;
+  }
+
+  function handleFilters(filter: string) {
+    const filterIsSelected = filters.filter(f => f === filter);
+
+    if (filterIsSelected && filterIsSelected.length > 0) {
+      setFilters(filters.filter(f => f !== filter));
+      return;
+    }
+
+    setFilters([...filters, filter]);
+  }
+
+  function filterListOfVans() {
+    let url = '';
+
+    filters.forEach(filter => {
+      url = url + `type=${filter}&`;
+    });
+
+    const urlWithoutLastCharacter = url.slice(0, -1);
+
+    api.get(`vans?${urlWithoutLastCharacter}`)
+      .then(res => setVans(res.data));
+  }
+
+  function clearFilters() {
+    setFilters([]);
+  }
+
+  const filterButtons = filtersType.map((filter) => (
+    <button
+      key={filter}
+      className={(filterIsActive(filter) ? `btn-${filter}-active` : '')}
+      onClick={() => handleFilters(filter)}
+      type='button'
+      id={`btn-${filter}`}>
+      {filter}
+    </button>
+  ));
+
+  const vanElements = vans.map((van) => (
+    <div className="van-item" key={van.id}>
+      <img src={van.imageUrl} alt={van.name} />
+
+      <div className="info">
+        <h3>{van.name}</h3>
+        <div className="price">
+          <p>${van.price}</p>
+          <p>/day</p>
+        </div>
+      </div>
+
+      <div className={`type-box type-box-${van.type}`}>
+        {van.type}
+      </div>
+
+    </div>
+  ));
+
+
+  return (
+    <section className='container vans-page'>
+
+      <h1 className='title'>Explore our van options</h1>
+
+      <div className="filters">
+        <div className="filter-btn">
+          {filterButtons}
+        </div>
+        <p className='clear-filters' onClick={clearFilters}>Clear filters</p>
+      </div>
+
+      <div className="van-list">
+        {vanElements}
+      </div>
+
+    </section>
+  )
+}
