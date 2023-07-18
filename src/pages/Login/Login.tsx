ee39e72cf141d23a033e07adbff9e713eab29d6c
Diego Pais <drborges.pais@gmail.com>
2023-07-18 17:50:08 -0300

    (us08): validating form with react-hook-for and zod

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,13 +1,82 @@
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
 import './style.css';
 
+
+
+const loginFormSchema = z.object({
+  email: z.string()
+    .nonempty('E-mail é obrigatório')
+    .email('formato de e-mail inválido')
+  ,
+  password: z.string()
+    .min(6, 'A senha deve ter no minimo 6 caracteres')
+  ,
+});
+
+type LoginForm = z.infer<typeof loginFormSchema>;
+
 export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<LoginForm>({
+    resolver: zodResolver(loginFormSchema)
+  });
+
+  function handleLoginSubmit(data: any) {
+    console.log('Form: ', data);
+  }
+
   return (
     <section className='container container-min-height login-page'>
       <h1>Sign in to your account</h1>
-      <form>
-        <input type="text" placeholder='Email address' />
-        <input type="password" placeholder='Password' />
-        <button type="submit" className='btn btn-primary'>Sign In</button>
+
+      <form onSubmit={handleSubmit(handleLoginSubmit)}>
+
+        <div className='form-group'>
+          <input
+            className={errors.email && 'input-error'}
+            type="text"
+            id='input-email'
+            placeholder='Email address'
+            {...register('email')}
+          />
+
+          {
+            errors.email
+            && <span className='input-error-message'>
+              {errors.email.message}
+            </span>
+          }
+        </div>
+
+        <div className='form-group'>
+          <input
+            className={errors.password && 'input-error'}
+            type="password"
+            id='input-password'
+            placeholder='Password'
+            {...register('password')}
+          />
+
+          {
+            errors.password
+            && <span className='input-error-message'>
+              {errors.password.message}
+            </span>
+          }
+        </div>
+
+        <button
+          type="submit"
+          className='btn btn-primary'>
+          Sign In
+        </button>
+
       </form>
       <p className='create-account'>
         Don’t have an account? <span>Create one now</span>
